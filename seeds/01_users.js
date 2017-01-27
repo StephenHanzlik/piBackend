667c55d1904733dad3a895a5412ada3bd3a3b616
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-01-27 10:31:52 -0700

    was missing array brackets on insert

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -7,7 +7,7 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       return Promise.all([
           // Inserts seed entries
-          knex('users').insert({
+          knex('users').insert([{
             id: 1,
             username: 'Eggs',
             password: '$2a$06$KrJ7ldWf2grQN.9SlIjRieJ3RMBe3KCB.7Euq0uU24iKrZ2yqU0Vy',
@@ -42,10 +42,12 @@ exports.seed = function(knex, Promise) {
             email: 'Steve@gmail.com',
             created_at: new Date('2016-12-06 14:26:16 UTC'),
             avatar_path: 'uploads/cliff.svg'
-          })
+          }])
         ])
         .then(() => {
-          return knex.raw("SELECT setval('users_id_seq', (SELECT MAX(id) FROM users))");
+          return knex.raw(
+            "SELECT setval('users_id_seq', (SELECT MAX(id) FROM users))"
+          );
         });
     });
 };
