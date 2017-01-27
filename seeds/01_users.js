7b00a4d07631f32b6510c6d8e3bcd38f21613e4b
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-01-26 23:12:26 -0700

    commit before pulling work done on py

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -13,7 +13,7 @@ exports.seed = function(knex, Promise) {
           password: '$2a$06$KrJ7ldWf2grQN.9SlIjRieJ3RMBe3KCB.7Euq0uU24iKrZ2yqU0Vy',
           email: 'dinkydinky@gmail.com',
           created_at: new Date('2016-12-06 14:26:16 UTC'),
-          avatar_path: '../img/bug.svg'
+          avatar_path: '../public/uploads/bug.svg'
         }),
         knex.raw("SELECT setval('users_id_seq', (SELECT MAX(id) FROM users))")
       ]);
